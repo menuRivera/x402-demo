beade31e633bfbfb4eb45cc9ea9d055564a1a264
Manuel Rivera <menu.rivera@gmail.com>
2026-02-17 16:05:35 -0600

    feat(frontend): evvmjs and reown integrations

---
+++ frontend/src/context/index.tsx
@@ -0,0 +1,51 @@
+import { createAppKit } from "@reown/appkit/react";
+import type { ReactNode } from "react";
+
+import { WagmiProvider } from "wagmi";
+import { sepolia } from "@reown/appkit/networks";
+import type { AppKitNetwork } from "@reown/appkit/networks";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId from https://dashboard.reown.com
+const projectId = "YOUR_PROJECT_ID";
+
+// 2. Create a metadata object - optional
+const metadata = {
+  name: "AppKit",
+  description: "AppKit Example",
+  url: "https://example.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+};
+
+// 3. Set the networks
+const networks: [AppKitNetwork, ...AppKitNetwork[]] = [sepolia];
+
+// 4. Create Wagmi Adapter
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: true,
+});
+
+// 5. Create modal
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+
+export function AppKitProvider({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AppKitProvider } from "./context";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AppKitProvider>
+      <App />
+    </AppKitProvider>
   </StrictMode>,
-)
+);
